9599675605c3660fb9dd1de8819ff2780f06e3c6
ma2en <m736743758@gmail.com>
2019-07-15 15:34:36 +0300

    first

---
+++ script.js
@@ -0,0 +1,36 @@
+var slidecount = 0;
+show();
+function show(){
+    var slides= document.getElementsByClassName("slide");
+    for (i=0; i<slides.length; i++){
+        slides[i].style.display= "none";
+    }
+    slidecount++;
+    if(slidecount>slides.length){
+        slidecount=1
+    }
+    slides[slidecount-1].style.display="block";
+    setTimeout(show, 4000)
+}
+function manualchang(n) {
+    show(slidecount += n);
+  }
+
+  const topbutton= document.getElementsByClassName("up-button");
+
+  topbutton.addEventListener("click",backtoup);
+
+
+  function backtoup(){
+    scroll(0,0)
+    
+  }
+  window.onscroll = function() {transbar()};
+
+  function transbar(){
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        document.getElementsByClassName("navbar").addClass("navigator");
+    }else{
+        document.getElementsByClassName("navbar").removeClass("navigator");
+    }
+  }
